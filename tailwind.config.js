e203d5818d79f78b47f90195aa259e513559204f
R12345r <rabiul.2114101311@mujonline.edu.in>
2022-12-17 10:07:43 +0530

    okdone

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
